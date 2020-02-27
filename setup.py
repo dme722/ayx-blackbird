405f6ae333f788f09a46715ab3b826620e5599a1
dellison <dellison@alteryx.com>
2020-02-27 08:34:44 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
+import os
 from setuptools import setup, find_packages
+
 from ayx_blackbird.version import __version__
+
+
+CLASSIFIERS = [
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering",
+    "Operating System :: Microsoft :: Windows"
+]
+
+SUMMARY = (__doc__ or "").split("\n")
+
+THIS_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(THIS_DIRECTORY, "README.md"), encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
 
 REQUIREMENTS = [
     "Click>=7.0",
@@ -15,6 +34,9 @@ setup(
     version=__version__,
     python_requires=">=3.6.0",
     description="Alteryx Designer Python SDK Abstraction Layer",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    classifiers=CLASSIFIERS,
     author="Drew Ellison",
     author_email="dme722@gmail.com",
     install_requires=REQUIREMENTS,
